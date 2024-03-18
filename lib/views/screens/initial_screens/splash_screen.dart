9519725a705bcd3dbcd1c3f3d0a2173ab3ecf3a2
kiranAppdid <kirankshirsagar.appdid@gmail.com>
2024-03-18 09:52:52 +0530

    splash updated

--- lib/views/screens/initial_screens/splash_screen.dart
+++ lib/views/screens/initial_screens/splash_screen.dart
@@ -6,6 +6,7 @@ import 'package:football_shuru/services/route_helper.dart';
 import 'package:football_shuru/services/theme.dart';
 import 'package:football_shuru/views/base/custom_image.dart';
 import 'package:football_shuru/views/screens/initial_screens/introduction_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -19,13 +20,17 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer.run(() {
-      Future.delayed(const Duration(seconds: 2), () {});
-      Navigator.pushReplacement(
-        context,
-        getCustomRoute(
-          child: const IntroductionScreen(),
-        ),
-      );
+      Future.delayed(const Duration(seconds: 2), () {
+        Navigator.pushReplacement(
+          context,
+          getCustomRoute(
+            type: PageTransitionType.fade,
+            duration: const Duration(milliseconds: 600),
+            child: const IntroductionScreen(),
+          ),
+        );
+      });
+
       /*if (Get.find<AuthController>().isLoggedIn()) {
         Get.find<AuthController>().getUserProfileData().then((value) {
           Future.delayed(const Duration(seconds: 2), () {
@@ -83,7 +88,7 @@ class _SplashScreenState extends State<SplashScreen> {
                   style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                       fontSize: 22.0,
                       fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(64, 66, 78, 1)),
+                      color: const Color.fromRGBO(64, 66, 78, 1)),
                 ),
                 Text(
                   AppConstants.appNamePart2,
@@ -98,7 +103,7 @@ class _SplashScreenState extends State<SplashScreen> {
               "SPORTS STARTS WITH US",
               style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                     fontSize: 11,
-                    color: Color.fromRGBO(43, 63, 108, 1),
+                    color: const Color.fromRGBO(43, 63, 108, 1),
                   ),
             ),
             const Spacer(),
